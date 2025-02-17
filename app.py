4b368ed3ce1f75049851b00e6c756f0f498d8f66
Sofia <sofia.capcha.g@uni.pe>
2025-02-17 16:47:24 -0500

    sdsd

--- app.py
+++ app.py
@@ -243,6 +243,19 @@ def verificar_base_datos():
     
     conn.commit()
     conn.close()
+ # Crear tabla de respuestas, permitiendo actualizar respuestas
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS respuestas (
+        id_usuario INTEGER,
+        pregunta TEXT NOT NULL,
+        respuesta TEXT,
+        PRIMARY KEY (id_usuario, pregunta),
+        FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario)
+    )
+    """)
+
+    conn.commit()
+    conn.close()
 
 verificar_base_datos()
 
