6162873fcd4df93e0d4706fb491cdefdc506d015
Sofia <sofia.capcha.g@uni.pe>
2025-02-17 00:41:09 -0500

    addse

--- app.py
+++ app.py
@@ -146,11 +146,8 @@ def encuesta():
 def logout():
     session.clear()
     return redirect(url_for("login")) 
-# 📌 Ruta para cerrar sesión
-@app.route("/logout")
-def logout():
-    session.clear()
-    return redirect(url_for("login"))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
