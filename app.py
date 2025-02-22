bbea30c3fbf9f0eb276575e8f5ca2566656abc64
Sofia <sofia.capcha.g@uni.pe>
2025-02-21 19:54:40 -0500

    cvx

--- app.py
+++ app.py
@@ -410,4 +410,5 @@ def ver_respuestas():
     return render_template("ver_respuestas.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+    
